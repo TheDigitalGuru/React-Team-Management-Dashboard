4a3ce3146772c30c2dcd87daf998a1d3c9f888a7
TheDigitalGuru <63559572+TheDigitalGuru@users.noreply.github.com>
2023-06-22 05:26:21 -0700

    added change that will show dashboard on initial display due to how GH-pages accesses this file

--- src/App.js
+++ src/App.js
@@ -30,6 +30,10 @@ function App() {
 						<Topbar setIsSidebar={setIsSidebar} />
 						<Routes>
 							<Route path='/' element={<Dashboard />} />
+							<Route
+								path='/React-Team-Management-Dashboard'
+								element={<Dashboard />}
+							/>
 							<Route path='/team' element={<Team />} />
 							<Route path='/contacts' element={<Contacts />} />
 							<Route path='/invoices' element={<Invoices />} />
