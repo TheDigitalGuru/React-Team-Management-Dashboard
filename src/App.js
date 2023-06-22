43c659d108198f048a01a9fd348de3a1990d8edb
TheDigitalGuru <63559572+TheDigitalGuru@users.noreply.github.com>
2023-06-22 04:04:16 -0700

    Uploading and Initializing my React-Team-Management-Dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Bar from './scenes/bar';
+import Calendar from './scenes/calendar';
+import Contacts from './scenes/contacts';
+import Dashboard from './scenes/dashboard';
+import FAQ from './scenes/faq';
+import Form from './scenes/form';
+import Geography from './scenes/geography';
+import Sidebar from './scenes/global/Sidebar';
+import Topbar from './scenes/global/Topbar';
+import Invoices from './scenes/invoices';
+import Line from './scenes/line';
+import Pie from './scenes/pie';
+import Team from './scenes/team';
+import { ColorModeContext, useMode } from './theme';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [theme, colorMode] = useMode();
+	const [isSidebar, setIsSidebar] = useState(true);
+
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className='app'>
+					<Sidebar isSidebar={isSidebar} />
+					<main className='content'>
+						<Topbar setIsSidebar={setIsSidebar} />
+						<Routes>
+							<Route path='/' element={<Dashboard />} />
+							<Route path='/team' element={<Team />} />
+							<Route path='/contacts' element={<Contacts />} />
+							<Route path='/invoices' element={<Invoices />} />
+							<Route path='/form' element={<Form />} />
+							<Route path='/bar' element={<Bar />} />
+							<Route path='/pie' element={<Pie />} />
+							<Route path='/line' element={<Line />} />
+							<Route path='/faq' element={<FAQ />} />
+							<Route path='/calendar' element={<Calendar />} />
+							<Route path='/geography' element={<Geography />} />
+						</Routes>
+					</main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
 }
 
 export default App;
